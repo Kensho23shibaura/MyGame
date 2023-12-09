b1a92b22162c0a066c7d2b6dc4c71ea0c48a5fad
Kensho23shibaura <153347784+Kensho23shibaura@users.noreply.github.com>
2023-12-09 15:09:39 +0900

    Add files via upload

---
+++ EnglishQuiz/english.js
@@ -0,0 +1,9 @@
+const questions = [
+    { word: 'appl', meaning: 'りん' },
+    { word: 'apple', meaning: 'りんご' },
+    { word: 'cat', meaning: 'ねこ' },
+    { word: 'sun', meaning: 'ひ' },
+    { word: 'book', meaning: 'ほん' },
+    { word: 'water', meaning: 'みず' },
+    { word: 'wood', meaning: 'き' }
+];
