8385486c571403d946e1f89c99885e73116957cc
BuildTools <unconfigured@null.spigotmc.org>
2023-12-18 23:33:28 +0900

    サンプルDB作成

---
+++ POS/sample_db.js
@@ -0,0 +1,43 @@
+const TabList = [
+    { type: "Tab", No: 0, tab: "A", name: "Menu TabA" },
+    { type: "Tab", No: 1, tab: "B", name: "Menu TabB" },
+    { type: "Tab", No: 2, tab: "C", name: "Menu TabC" },
+    { type: "Tab", No: 3, tab: "D", name: "Menu TabD" },
+]
+const ItemList = [
+    { type: "Menu", No: 0, tab: "A", name: "さくらんぼ", price: 100, img: "img/menuA.jpg" },
+    { type: "Menu", No: 1, tab: "A", name: "いちご", price: 100, img: "img/menuA.jpg" },
+    { type: "Menu", No: 2, tab: "A", name: "ぶどう", price: 100, img: "img/menuA.jpg" },
+    { type: "Menu", No: 3, tab: "A", name: "デコポン", price: 100, img: "img/menuA.jpg" },
+    { type: "Menu", No: 4, tab: "A", name: "かき", price: 100, img: "img/menuA.jpg" },
+    { type: "Menu", No: 5, tab: "A", name: "りんご", price: 100, img: "img/menuA.jpg" },
+    { type: "Menu", No: 6, tab: "B", name: "なし", price: 100, img: "img/menuB.jpg" },
+    { type: "Menu", No: 7, tab: "B", name: "もも", price: 100, img: "img/menuB.jpg" },
+    { type: "Menu", No: 8, tab: "B", name: "パイナップル", price: 100, img: "img/menuB.jpg" },
+    { type: "Menu", No: 9, tab: "C", name: "メロン", price: 100, img: "img/menuC.jpg" },
+    { type: "Menu", No: 10, tab: "C", name: "スイカ", price: 100, img: "img/menuC.jpg" },
+]
+const TableList = [
+    { order_id: 1, created_at: "2023/12/17 15:00:00" },
+    { order_id: 2, created_at: "2023/12/17 16:00:00" },
+    { order_id: 3, created_at: "2023/12/17 17:00:00" },
+    { order_id: 4, created_at: "2023/12/17 18:00:00" },
+    { order_id: 5, created_at: "2023/12/17 15:00:00" },
+    { order_id: 6, created_at: "2023/12/17 16:00:00" },
+    { order_id: 7, created_at: "2023/12/17 17:00:00" },
+    { order_id: 8, created_at: "2023/12/17 18:00:00" },
+    { order_id: 9, created_at: "2023/12/17 15:00:00" },
+    { order_id: 10, created_at: "2023/12/17 16:00:00" },
+    { order_id: 11, created_at: "2023/12/17 17:00:00" },
+    { order_id: 12, created_at: "2023/12/17 18:00:00" },
+]
+const OrderList = [
+    { No: 0, order_id: 1, item_no: 0, name: "さくらんぼ", amount: 1, created_at: "2023/12/17 15:20:00", updated_at: "2023/12/17 15:20:00", price: 100, img: "img/menuA.jpg" },
+    { No: 1, order_id: 2, item_no: 1, name: "いちご", amount: 2, created_at: "2023/12/17 15:21:00", updated_at: "2023/12/17 15:21:00", price: 100, img: "img/menuA.jpg" },
+    { No: 2, order_id: 1, item_no: 4, name: "かき", amount: 3, created_at: "2023/12/17 15:23:00", updated_at: "2023/12/17 15:23:00", price: 100, img: "img/menuA.jpg" },
+    { No: 3, order_id: 4, item_no: 3, name: "デコポン", amount: 2, created_at: "2023/12/17 15:25:00", updated_at: "2023/12/17 15:27:00", price: 100, img: "img/menuA.jpg" },
+    { No: 4, order_id: 3, item_no: 1, name: "いちご", amount: 2, created_at: "2023/12/17 15:27:00", updated_at: "2023/12/17 15:27:00", price: 100, img: "img/menuA.jpg" },
+    { No: 5, order_id: 2, item_no: 6, name: "なし", amount: 1, created_at: "2023/12/17 15:29:00", updated_at: "2023/12/17 15:29:00", price: 100, img: "img/menuB.jpg" },
+    { No: 6, order_id: 1, item_no: 5, name: "りんご", amount: 3, created_at: "2023/12/17 15:30:00", updated_at: "2023/12/17 15:32:00", price: 100, img: "img/menuA.jpg" },
+    { No: 7, order_id: 2, item_no: 0, name: "さくらんぼ", amount: 1, created_at: "2023/12/17 15:35:00", updated_at: "2023/12/17 15:35:00", price: 100, img: "img/menuA.jpg" },
+]
